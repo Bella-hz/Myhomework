1c0388c56a0bb9929dfecaaea1e845f72f5767b6
wangyanan <wangyanan@deepwise.com>
2020-06-27 18:55:36 +0800

    pytest实战二作业提交

--- calculator/calculator_test.py
+++ calculator/calculator_test.py
@@ -2,9 +2,11 @@
 # coding = utf-8
 # __author__ = 'wyn'
 # Filename: calculator_test.py
+from pytest import approx
 import pytest
 import yaml
 import os
+
 
 work_path = os.path.abspath(os.path.dirname(__file__))
 testdata_file = os.path.join(work_path, 'calculator_testdata.yaml')
@@ -13,26 +15,32 @@ with open(testdata_file) as file:
 
 
 class TestCal:
+    @pytest.mark.run(order=1)
+    @pytest.mark.dependency(name='add')
+    @pytest.mark.usefixtures('cmd_option')
     @pytest.mark.parametrize(('a', 'b', 'result'), testdata['add'])
-    def test_add(self, create_cal, a, b, result):
-        if isinstance(a, int) and 1 == len({type(a), type(b), type(result)}):
-            print(f'{a}+{b}={result}')
-            assert create_cal.add(a, b) == result
+    def test_add(self, create_cal, a, b, result, cmd_option):
+        print(f'{a}+{b}={result}')
+        print(f"当前环境{cmd_option}")
+        assert create_cal.add(a, b) == approx(result)
 
+    @pytest.mark.run(order=2)
+    @pytest.mark.dependency(depends=["add"])
     @pytest.mark.parametrize(('a', 'b', 'result'), testdata['sub'])
     def test_sub(self, create_cal, a, b, result):
-        if isinstance(a, int) and 1 == len({type(a), type(b), type(result)}):
-            print(f'{a}-{b}={result}')
-            assert create_cal.sub(a, b) == result
+        print(f'{a}-{b}={result}')
+        assert create_cal.sub(a, b) == approx(result)
 
+    @pytest.mark.run(order=3)
+    @pytest.mark.dependency(name='mul')
     @pytest.mark.parametrize(('a', 'b', 'result'), testdata['mul'])
     def test_mul(self, create_cal, a, b, result):
-        if isinstance(a, int) and 1 == len({type(a), type(b), type(result)}):
-            print(f'{a}*{b}={result}')
-            assert create_cal.mul(a, b) == result
+        print(f'{a}*{b}={result}')
+        assert create_cal.mul(a, b) == approx(result)
 
+    @pytest.mark.run(order=4)
+    @pytest.mark.dependency(depends=["mul"])
     @pytest.mark.parametrize(('a', 'b', 'result'), testdata['div'])
     def test_div(self, create_cal, a, b, result):
-        if isinstance(a, int) and 1 == len({type(a), type(b), type(result)}):
-            print(f'{a}/{b}={result}')
-            assert create_cal.div(a, b) == result
+        print(f'{a}/{b}={result}')
+        assert create_cal.div(a, b) == approx(result)
--- calculator/conftest.py
+++ calculator/conftest.py
@@ -2,8 +2,9 @@
 # coding = utf-8
 # __author__ = 'wyn'
 # Filename: conftest.py
-import pytest
 from calculator.calculator import Calculate
+import pytest
+import os
 
 
 @pytest.fixture()
@@ -12,3 +13,23 @@ def create_cal():
     print('开始计算')
     yield cal
     print('计算结束')
+
+
+def pytest_addoption(parser):
+    my_group = parser.getgroup('hogwarts')
+    my_group.addoption("--env", default='test', dest='env', help='set your run env')
+
+
+@pytest.fixture(scope='session')
+def cmd_option(request):
+    my_env = request.config.getoption("--env", default='test')
+    if my_env == 'test':
+        os.environ['ENV'] = 'test'
+        print("获取test数据")
+    elif my_env == 'dev':
+        os.environ['ENV'] = 'dev'
+        print("获取dev数据")
+    elif my_env == 'st':
+        os.environ['ENV'] = 'st'
+        print("获取st数据")
+    return my_env
